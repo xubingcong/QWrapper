1240891773cb3268991de55b482321ec88239b92
xubingcong <xubingcong@126.com>
2014-06-09 21:59:57 +0800

    Update Wrapper_gjsairw6001.java

--- src/Wrapper_gjsairw6001.java
+++ src/Wrapper_gjsairw6001.java
@@ -22,7 +22,6 @@ import com.qunar.qfwrapper.bean.booking.BookingResult;
 import com.qunar.qfwrapper.bean.search.FlightDetail;
 import com.qunar.qfwrapper.bean.search.FlightSearchParam;
 import com.qunar.qfwrapper.bean.search.FlightSegement;
-import com.qunar.qfwrapper.bean.search.OneWayFlightInfo;
 import com.qunar.qfwrapper.bean.search.ProcessResultInfo;
 import com.qunar.qfwrapper.bean.search.RoundTripFlightInfo;
 import com.qunar.qfwrapper.constants.Constants;
@@ -220,7 +219,7 @@ public class Wrapper_gjsairw6001 implements QunarCrawler{
 								"requiredError").trim();
 				// 截取出航班信息
 				String[] array = span.split("~");
-				String code = "11CA" + array[8];
+				String code = array[7] + array[8];
 				String depDate = array[12];
 				String arrDate = array[14];
 				// 截取出票价信息
@@ -260,7 +259,7 @@ public class Wrapper_gjsairw6001 implements QunarCrawler{
 							"requiredError").trim();
 					// 截取航班信息
 					String[] ret_array = ret_span.split("~");
-					String ret_code = "11CA" + ret_array[8];
+					String ret_code = ret_array[7] + ret_array[8];
 					String ret_depDate = ret_array[12];
 					String ret_arrDate = ret_array[14];
 					// 截取出票价信息
